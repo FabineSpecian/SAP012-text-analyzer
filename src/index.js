8a728d0e73b004e078116035c64400fb0b8c0600
Fabine Specian <fabine.specian@gmail.com>
2023-12-19 21:49:23 +0000

    Em desenvolvimento do DOM e JavaScript

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-import analyzer from './analyzer.js';
+import analyzer from "./analyzer.js";
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+ const textarea = document.querySelector('[name="user-input"]'); 
+
+ const button = document.getElementById("reset-button"); 
+
+ const wordCount = document.querySelector('[data-testid="word-count"]'); 
+
+ const characterCount = document.querySelector('[data-testid="character-count"]'); 
+
+ const characterNoSpacesCount = document.querySelector('[data-testid="character-no-spaces-count"]');
+
+ const numberCount = document.querySelector('[data-testid="number-count"]'); 
+
+ const numberSum = document.querySelector('[data-testid="number-sum"]'); 
+
+ const wordLengthAverage = document.querySelector('[data-testid="word-length-average"]');
+
+ 
+function allMetricsUpdate() { 
+  const text = textarea.value; 
+  wordCount.textContent = "Palavras: " + analyzer.getWordCount(text);
+  characterCount.textContent = "Caracteres: " + analyzer.getCharacterCount(text);
+  characterNoSpacesCount.textContent = "Caracteres sem espaços e símbolos: " + analyzer.getCharacterCountExcludingSpaces(text);
+  numberCount.textContent = "Números: " + analyzer.getNumberCount(text);
+  numberSum.textContent = "Soma dos números: " + analyzer.getNumberSum(text);
+  wordLengthAverage.textContent = "Comprimento médio: " + analyzer.getAverageWordLength(text);
+}
+
+function clearAllMetricsAndText() {  
+  textarea.value = "";
+  wordCount.textContent = "Palavras: 0"; 
+  characterCount.textContent = "Caracteres: 0";
+  characterNoSpacesCount.textContent = "Caracteres sem espaços e símbolos: 0";
+  numberCount.textContent = "Números: 0";
+  numberSum.textContent = "Soma dos números: 0";
+  wordLengthAverage.textContent = "Comprimento médio:  0";
+}
+
+
+textarea.addEventListener("keyup", allMetricsUpdate); 
+
+button.addEventListener("click", clearAllMetricsAndText); 
+
+textarea.addEventListener('input', allMetricsUpdate);
