88a3cdd4fe9f37f6653c5889f2313c41bfa70d22
Fabine Specian <fabine.specian@gmail.com>
2023-12-20 11:40:02 +0000

    Em desenvolvimento

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,79 +1,23 @@
-const analyzer = {
+const analyzer = {  
   getWordCount: (text) => {
-    const alphanumericPattern = /[a-zA-Z0-9_]/; /*Padrão alfanumérico, corresponde a qualquer caractere (letra ou número) ou sublinhado.*/
-    if (text.trim().length === 0 || !alphanumericPattern.test(text)) {
-      return 0; /*Verifica se o texto está vazio ou se não contém caracteres alfanuméricos. Se qualquer uma dessas condições for verdadeira, a função retorna 0, indicando que não há palavras. */
-    }
-    const countWordsInText = text.trim().split(" "); /* Remove espaços em branco no início e no final da string e, em seguida, divide o texto em palavras com base nos espaços em branco usando o método split. */
-    return countWordsInText.length; 
-  },/* Retorna o número de palavras contadas. Isso é feito obtendo o comprimento do array resultante da divisão do texto em palavras. */
-
-
+   
+  },
   getCharacterCount: (text) => {
-    return text.length;
+   
   },
-
-
   getCharacterCountExcludingSpaces: (text) => {
-    const alphanumericPattern = /[a-zA-Z0-9_]/;
-    if (text.trim().length === 0 || !alphanumericPattern.test(text)) {
-      return 0;
-    }
-    const regex = /[ ,." ']/g; 
-    const totalCharactere = text.replace(regex, "");
-    return totalCharactere.length;
+    
   },
-  
-  
+  getAverageWordLength: (text) => {    
+    
+  },
   getNumberCount: (text) => {
-    const alphanumericPattern = /[a-zA-Z0-9_]/;
-    if (text.trim().length === 0 || !alphanumericPattern.test(text)) {
-      return 0;
-    }
-    const regex = /[.,!?]$/g;
-    const textWithoutPunctuation = text.replace(regex, "");
-    const wordsAndNumbersInText = textWithoutPunctuation.trim().split(" ");
-    let qntNumbers = 0;
-    wordsAndNumbersInText.forEach((word) => {
-      if (typeof parseInt(word) === "number" && !isNaN(word)) {
-        qntNumbers = qntNumbers + 1;
-      }
-    });
-    return qntNumbers;
+  
   },
-
   getNumberSum: (text) => {
-    const regex = /[.,!?]$/g;
-    const textWithoutPunctuation = text.replace(regex, "");
-    const wordsAndNumbersInText = textWithoutPunctuation.trim().split(" ");
-    let numberSum = 0;
-    wordsAndNumbersInText.forEach((word) => {
-      if (
-        (typeof parseInt(word) === "number" ||
-          typeof parseFloat(word) === "number") &&
-        !isNaN(word)
-      ) {
-        numberSum = numberSum + +word; 
-      }
-    });
-    return numberSum;
-  },
-
-  getAverageWordLength: (text) => {
-    const alphanumericPattern = /[a-zA-Z0-9_]/;
-    if (text.trim().length === 0 || !alphanumericPattern.test(text)) {
-      return 0;
-    }
-    const wordsAndNumbersInText = text.trim().split(" ");
-    let wordsLength = 0;
-    for (let index = 0; index < wordsAndNumbersInText.length; index++) {
-      const word = wordsAndNumbersInText[index];
-      wordsLength = wordsLength + word.length;
-    }
-
-    const media = wordsLength / wordsAndNumbersInText.length;
-    return parseFloat(media.toFixed(2));
+  
   },
 };
+
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,44 +1,10 @@
-import analyzer from "./analyzer.js";
-
- const textarea = document.querySelector('[name="user-input"]'); 
-
- const button = document.getElementById("reset-button"); 
-
- const wordCount = document.querySelector('[data-testid="word-count"]'); 
-
- const characterCount = document.querySelector('[data-testid="character-count"]'); 
-
- const characterNoSpacesCount = document.querySelector('[data-testid="character-no-spaces-count"]');
-
- const numberCount = document.querySelector('[data-testid="number-count"]'); 
-
- const numberSum = document.querySelector('[data-testid="number-sum"]'); 
-
- const wordLengthAverage = document.querySelector('[data-testid="word-length-average"]');
-
- 
-function allMetricsUpdate() { 
-  const text = textarea.value; 
-  wordCount.textContent = "Palavras: " + analyzer.getWordCount(text);
-  characterCount.textContent = "Caracteres: " + analyzer.getCharacterCount(text);
-  characterNoSpacesCount.textContent = "Caracteres sem espaços e símbolos: " + analyzer.getCharacterCountExcludingSpaces(text);
-  numberCount.textContent = "Números: " + analyzer.getNumberCount(text);
-  numberSum.textContent = "Soma dos números: " + analyzer.getNumberSum(text);
-  wordLengthAverage.textContent = "Comprimento médio: " + analyzer.getAverageWordLength(text);
-}
-
-function clearAllMetricsAndText() {  
-  textarea.value = "";
-  wordCount.textContent = "Palavras: 0"; 
-  characterCount.textContent = "Caracteres: 0";
-  characterNoSpacesCount.textContent = "Caracteres sem espaços e símbolos: 0";
-  numberCount.textContent = "Números: 0";
-  numberSum.textContent = "Soma dos números: 0";
-  wordLengthAverage.textContent = "Comprimento médio:  0";
-}
+import analyzer from './analyzer.js';
 
 
-textarea.addEventListener("keyup", allMetricsUpdate); 
-textarea.addEventListener('input', allMetricsUpdate);
-button.addEventListener("click", clearAllMetricsAndText); 
+//const textarea = document.getElementByName("user-input");
 
+
+
+
+
+//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
