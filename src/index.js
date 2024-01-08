db3aa754fa7ec60f9714c76d9ca91863ef209965
Fabine Specian <fabine.specian@gmail.com>
2024-01-08 10:08:04 +0000

    JS finalizado, todos os testes rodando

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import analyzer from './analyzer.js';
 const resetButton = document.getElementById('reset-button');
 const textarea = document.querySelector('[name="user-input"]');
 const wordCount = document.querySelector('[data-testid="word-count"]');
-const characterCount = document.querySelector ('[data-testid="character-count"]');
-const characterNoSpacesCount = document.querySelector ('[data-testid="character-no-spaces-count"]');
-const numberCount = document.querySelector ('[data-testid="number-count"]');
-const numberSum = document.querySelector ('[data-testid="number-sum"]');
-const averageWordLength = document.querySelector ('[data-testid="word-lenght-average"]');
+const characterCount = document.querySelector('[data-testid="character-count"]');
+const characterNoSpacesCount = document.querySelector('[data-testid="character-no-spaces-count"]');
+const numberCount = document.querySelector('[data-testid="number-count"]');
+const numberSum = document.querySelector('[data-testid="number-sum"]');
+const averageWordLength = document.querySelector('[data-testid="word-length-average"]');
 
 
 resetButton.addEventListener("click", limparTexto);
@@ -24,13 +24,13 @@ function limparTexto() {
   averageWordLength.textContent = "Comprimento médio: 0";
 }
 
-textarea.addEventListener("keyup", atualizarValores); 
+textarea.addEventListener("keyup", atualizarValores);
 
 function atualizarValores() {
   const text = textarea.value;
   wordCount.textContent = "Palavras:" + analyzer.getWordCount(text);
-  characterCount.textContent = "Caracteres:" + analyzer. getCharacterCount(text);
-  characterNoSpacesCount.textContent= "Caracteres sem espaços e símbolos:" + analyzer.getCharacterCountExcludingSpaces(text);
+  characterCount.textContent = "Caracteres:" + analyzer.getCharacterCount(text);
+  characterNoSpacesCount.textContent = "Caracteres sem espaços e símbolos:" + analyzer.getCharacterCountExcludingSpaces(text);
   numberCount.textContent = "Números:" + analyzer.getNumberCount(text);
   numberSum.textContent = "Soma dos números:" + analyzer.getNumberSum(text);
   averageWordLength.textContent = "Comprimento médio:" + analyzer.getAverageWordLength(text);
